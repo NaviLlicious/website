1e3fcc36bbbd96f16cbc636a344d77f7afea6992
NaviLlicious <ianscottysullivan@hotmail.com>
2017-11-06 15:29:33 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+
+<header>
+	
+	<link rel="stylesheet" href="css/style.css">
+	
+	<div id="navbar">
+			
+			<ul class="navbarLeft">
+				
+					<li><a href="index.php">Home</a></li>
+					<li><a href="news.php">News</a></li>
+					<li><a href="index.php">Extra</a></li>
+				
+			</ul>
+			
+			<ul class="login">
+					
+				<li><a href="login.php">NaviLlicious</a></li>
+						
+			</ul>
+		
+	</div>
+	
+</header>
+
+<body>
+
+</body>
+
+</html>
